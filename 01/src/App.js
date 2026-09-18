09407ceb6c9e72bb4b1dbd6d2c19df447dc05edb
Bhavishya289 <arabhavishyasingh2006@gmail.com>
2026-09-18 21:58:47 +0530

    remove unused react router import

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import { motion } from "framer-motion";
 import Login from "./Components/Login/Login";
